53528d9a5869bcaa648bd8d75e6d304491f6090d
emonsec <root@emonsec.com>
2021-02-22 15:55:01 +0800

    2021-02-22 15:55:01

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# 清理后再生成Jekyll网页
+jekyll clean
+jekyll build
+# 在网页目录建立git
+cd _site
+git init
+# 添加VPS上的裸仓库作为远程仓库
+git remote add jekyll git@github.com:bugj/bugj.github.io.git
+
+# 提交并推送, 这里的commit信息我定的是提交时间
+git add .
+commit_time=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "${commit_time}"
+git push -f jekyll master:main
+# 删除本地网页文件夹
+cd ../
+rm -rf _site
+# 将Jekyll工程文件提交到GitHub
+git add .
+git commit -m "$1"
+git push github master:main
